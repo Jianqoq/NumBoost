Fixed all backward calculation memory leak problems

test/functionality_test/test_backward.py
@@ -199,20 +199,6 @@ def test_C_Tensor_arcsin_backward(array, grad):
 
 
 @pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
-def test_C_Tensor_arccos_backward(array, grad):
-    autograd_grad_operands1 = Tensor(array, True)
-    autograd_grad = grad
-    torch_operands1 = torch.tensor(array, requires_grad=True)
-    torch_grad = torch.tensor(grad)
-    result1 = core.arccos(autograd_grad_operands1)
-    result2 = torch.arccos(torch_operands1)
-    result1.backward(autograd_grad)
-    result2.backward(torch_grad)
-    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True),\
-        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
-
-
-@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
 def test_C_Tensor_arctan_backward(array, grad):
     autograd_grad_operands1 = Tensor(array, True)
     autograd_grad = grad
@@ -234,20 +220,6 @@ def test_C_Tensor_arcsinh_backward(array, grad):
     torch_grad = torch.tensor(grad)
     result1 = core.arcsinh(autograd_grad_operands1)
     result2 = torch.arcsinh(torch_operands1)
-    result1.backward(autograd_grad)
-    result2.backward(torch_grad)
-    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
-        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
-
-
-@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
-def test_C_Tensor_power_backward(array, grad):
-    autograd_grad_operands1 = Tensor(array, True)
-    autograd_grad = grad
-    torch_operands1 = torch.tensor(array, requires_grad=True)
-    torch_grad = torch.tensor(grad)
-    result1 = core.power(autograd_grad_operands1, 3)
-    result2 = torch.pow(torch_operands1, 3)
     result1.backward(autograd_grad)
     result2.backward(torch_grad)
     assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
@@ -310,4 +282,239 @@ def test_C_Tensor_log_backward(array, grad):
         f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
 
 
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_power_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.power(autograd_grad_operands1, 3)
+    result2 = torch.pow(torch_operands1, 3)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
 
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_sin_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.sin(autograd_grad_operands1)
+    result2 = torch.sin(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_cos_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.cos(autograd_grad_operands1)
+    result2 = torch.cos(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_tan_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.tan(autograd_grad_operands1)
+    result2 = torch.tan(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_arcsin_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.arcsin(autograd_grad_operands1)
+    result2 = torch.arcsin(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_arccos_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.arccos(autograd_grad_operands1)
+    result2 = torch.arccos(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_arctan_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.arctan(autograd_grad_operands1)
+    result2 = torch.arctan(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_sinh_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.sinh(autograd_grad_operands1)
+    result2 = torch.sinh(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_cosh_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.cosh(autograd_grad_operands1)
+    result2 = torch.cosh(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_tanh_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.cosh(autograd_grad_operands1)
+    result2 = torch.cosh(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_arcsinh_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.arcsinh(autograd_grad_operands1)
+    result2 = torch.arcsinh(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_arccosh_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.arccosh(autograd_grad_operands1)
+    result2 = torch.arccosh(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_arctanh_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.arctanh(autograd_grad_operands1)
+    result2 = torch.arctanh(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_exp_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.exp(autograd_grad_operands1)
+    result2 = torch.exp(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_log_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.log(autograd_grad_operands1)
+    result2 = torch.log(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_log10_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.log10(autograd_grad_operands1)
+    result2 = torch.log10(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"
+
+
+@pytest.mark.parametrize("array, grad", [(np.array([[1.], [2.], [3.]]), np.random.random((3, 1)))])
+def test_C_Tensor_sqrt_backward(array, grad):
+    autograd_grad_operands1 = Tensor(array, True)
+    autograd_grad = grad
+    torch_operands1 = torch.tensor(array, requires_grad=True)
+    torch_grad = torch.tensor(grad)
+    result1 = core.sqrt(autograd_grad_operands1)
+    result2 = torch.sqrt(torch_operands1)
+    result1.backward(autograd_grad)
+    result2.backward(torch_grad)
+    assert np.allclose(autograd_grad_operands1.grad, torch_operands1.grad.numpy(), equal_nan=True), \
+        f"correct: {torch_operands1.grad.numpy()} | got: {autograd_grad_operands1.grad}"