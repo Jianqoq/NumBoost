28071ca6d47c5e9db1a804151ff8438d435f6703
jianjian <ljj1849532909@gmail.com>
2023-07-08 21:31:25 -0400

    Added more functions

---
+++ src/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, Extension
+import numpy
+import os
+from setuptools.command.build_ext import build_ext as _build_ext
+import platform
+if platform.system() == 'Windows':
+    args = ['/Ox']
+else:
+    args = ['-O3']
+
+mymodule = Extension('tensor',
+                     sources=['tensor.c', 'operators.c', 'backward_fn.c', 'stack.c', 'set_Tensor_properties.c', 'methods.c', 'core.c'],
+                     include_dirs=[numpy.get_include()],
+                     language='c',
+                     extra_compile_args=args,
+                     define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')])
+
+mymodule2 = Extension('core',
+                     sources=['tensor.c', 'operators.c', 'backward_fn.c', 'stack.c', 'set_Tensor_properties.c','methods.c', 'core.c'],
+                     include_dirs=[numpy.get_include()],
+                     language='c',
+                     extra_compile_args=args,
+                     define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')])
+
+class build_ext(_build_ext):
+    def get_ext_fullpath(self, ext_name):
+        filename = _build_ext.get_ext_filename(self, ext_name)
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+
+
+setup(name='autograd_C',
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[mymodule, mymodule2])
