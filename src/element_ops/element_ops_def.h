bdb66ee9bdf12f1e0fe3a7840439bfe45021d321
jianjian <ljj1849532909@gmail.com>
2023-09-11 01:05:14 -0400

    Reconstructing project structure

--- src/element_ops/element_ops_def.h
+++ src/element_ops/element_ops_def.h
@@ -1,6 +1,6 @@
 #ifndef _ELEMENT_OPS_DEF_H
 #define _ELEMENT_OPS_DEF_H
-#include "element_ops_impl.h"
+#include "elementwise_kernels.h"
 
 #define Register_ElementWise_Operation_Array(name, sufix)                      \
   PyObject *(*name##_operations##sufix[])(PyObject *, PyObject **, int) = {    \
--- src/python_magic/python_math_magic.h
+++ src/python_magic/python_math_magic.h
@@ -9,6 +9,7 @@ PyObject *create_tensor(Tensor *tensor, PyObject *other, PyObject *data,
 
 PyObject *tensor_empty(PyObject *data);
 
+/*Tensor magic methods*/
 PyObject *tensor_add(PyObject *self, PyObject *other);
 PyObject *tensor_iadd(PyObject *self, PyObject *other);
 PyObject *tensor_mul(PyObject *self, PyObject *other);
